84f47b8f0b13e478b1452a4b12437d7e1afa1fbe
Tasnia Kamal <tasnia@brismo.com>
2019-06-05 13:19:27 +0100

    Now add_arrays function adds arrays that were passed to it of a known array length

--- add_arrays.cpp
+++ add_arrays.cpp
@@ -8,17 +8,23 @@ using namespace std;
 
 // passing arrays as arguements
 // don't pass whole array, pass address of first element
-void AddArrays::add_arrays(int argc[], int argv[]) {
-	int v1[3], v2[3], v3[3];
+void AddArrays::add_arrays(int argc[], int argv[]) 
+{
+	int v3[3];
 
+	// print arrays passed in.
+	printf("\nArray 1 :\n");
+	for (int i = 0; i < 3; i++)
+		printf("%d\n", argc[i]);
+
+	printf("\nArray 2 :\n");
+	for (int i = 0; i < 3; i++)
+		printf("%d\n", argv[i]);
+
+	// loop through each array and add.
 	for (int i = 0; i < 3; i++) {
-		printf("Type a number for v1 :\t");
-		scanf_s("%d", &v1[i]);
-
-		printf("Type a number for v2 :\t");
-		scanf_s("%d", &v2[i]);
 		// Add here
-		v3[i] = v1[i] + v2[i]; // Mind you that this could lead to integer overflow.
+		v3[i] = argc[i] + argv[i]; // Mind you that this could lead to integer overflow.
 	}
 
 	printf("\nResult Arr :\n");
